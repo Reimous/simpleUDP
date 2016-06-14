cb9bd0a2cb1e39652685a7e297d22495a8a4e3d9
Reimous <victor_r88@hotmail.com>
2016-06-15 00:47:23 +0200

    Añadido proyecto
    1

---
+++ Form1.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Me.TBTo = New System.Windows.Forms.TextBox
+        Me.TBPort = New System.Windows.Forms.TextBox
+        Me.TBSend = New System.Windows.Forms.TextBox
+        Me.TBRcv = New System.Windows.Forms.TextBox
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'TBTo
+        '
+        Me.TBTo.Location = New System.Drawing.Point(49, 27)
+        Me.TBTo.Name = "TBTo"
+        Me.TBTo.Size = New System.Drawing.Size(100, 20)
+        Me.TBTo.TabIndex = 0
+        Me.TBTo.Text = "192.168.1.50"
+        '
+        'TBPort
+        '
+        Me.TBPort.Location = New System.Drawing.Point(49, 63)
+        Me.TBPort.Name = "TBPort"
+        Me.TBPort.Size = New System.Drawing.Size(100, 20)
+        Me.TBPort.TabIndex = 0
+        Me.TBPort.Text = "8081"
+        '
+        'TBSend
+        '
+        Me.TBSend.Location = New System.Drawing.Point(49, 100)
+        Me.TBSend.Name = "TBSend"
+        Me.TBSend.Size = New System.Drawing.Size(100, 20)
+        Me.TBSend.TabIndex = 0
+        Me.TBSend.Text = "Prueba"
+        '
+        'TBRcv
+        '
+        Me.TBRcv.Location = New System.Drawing.Point(49, 173)
+        Me.TBRcv.Name = "TBRcv"
+        Me.TBRcv.Size = New System.Drawing.Size(100, 20)
+        Me.TBRcv.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(49, 139)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Send"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(588, 476)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TBRcv)
+        Me.Controls.Add(Me.TBSend)
+        Me.Controls.Add(Me.TBPort)
+        Me.Controls.Add(Me.TBTo)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TBTo As System.Windows.Forms.TextBox
+    Friend WithEvents TBPort As System.Windows.Forms.TextBox
+    Friend WithEvents TBSend As System.Windows.Forms.TextBox
+    Friend WithEvents TBRcv As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Net
+Imports System.Text.Encoding
+
+Public Class Form1
+    Dim publisher As New Sockets.UdpClient(0)
+    Dim subscriber As New Sockets.UdpClient(8081)
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        publisher.Connect(TBTo.Text, TBPort.Text)
+        Dim sendbytes() As Byte = ASCII.GetBytes(TBSend.Text)
+        publisher.Send(sendbytes, sendbytes.Length)
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        subscriber.Client.ReceiveTimeout = 100
+        subscriber.Client.Blocking = False
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Try
+            Dim ep As IPEndPoint = New IPEndPoint(IPAddress.Any, 0)
+            Dim rcvbytes() As Byte = subscriber.Receive(ep)
+            TBRcv.Text = ASCII.GetString(rcvbytes)
+        Catch ex As Exception
+        End Try
+    End Sub
+
+End Class
